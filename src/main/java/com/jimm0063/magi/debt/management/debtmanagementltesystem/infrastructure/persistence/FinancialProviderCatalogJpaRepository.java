c8bc6a4eac8a454fefa3edec859fb31007ca7054
jimm0063 <jimm0063@gmail.com>
2025-07-16 13:03:21 -0600

    import optimization

--- src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/infrastructure/persistence/FinancialProviderCatalogJpaRepository.java
+++ src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/infrastructure/persistence/FinancialProviderCatalogJpaRepository.java
@@ -1,6 +1,5 @@
 package com.jimm0063.magi.debt.management.debtmanagementltesystem.infrastructure.persistence;
 
-import com.jimm0063.magi.debt.management.debtmanagementltesystem.domain.model.FinancialProviderCatalog;
 import com.jimm0063.magi.debt.management.debtmanagementltesystem.infrastructure.entity.FinancialProviderCatalogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
