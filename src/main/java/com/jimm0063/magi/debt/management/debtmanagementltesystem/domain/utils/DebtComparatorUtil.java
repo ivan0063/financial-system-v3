3bb9c73ac81ac4c65053c22bad63b4c9c22c07f4
Javier Ivan Martinez Mateos <ivan0063@outlook.com>
2025-08-23 15:33:52 -0600

    Actualizar DebtComparatorUtil.java
    It was deleted the description comparación between debts

--- src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/domain/utils/DebtComparatorUtil.java
+++ src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/domain/utils/DebtComparatorUtil.java
@@ -4,8 +4,8 @@ import com.jimm0063.magi.debt.management.debtmanagementltesystem.domain.model.De
 
 public class DebtComparatorUtil {
     public static boolean compareDebts(Debt debt1, Debt debt2) {
-        return debt1.getDescription().equals(debt2.getDescription()) &&
-                debt1.getOperationDate().equals(debt2.getOperationDate()) &&
+        //debt1.getDescription().equals(debt2.getDescription()) &&
+        return debt1.getOperationDate().equals(debt2.getOperationDate()) &&
                 debt1.getMaxFinancingTerm().equals(debt2.getMaxFinancingTerm()) &&
                 debt1.getMonthlyPayment().equals(debt2.getMonthlyPayment());
     }
