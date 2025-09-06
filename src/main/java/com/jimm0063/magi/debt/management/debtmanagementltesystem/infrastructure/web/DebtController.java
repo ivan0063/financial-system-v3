119590e137b91a6d19d33a0af24fa9839f8868a3
jimm0063 <jimm0063@gmail.com>
2025-09-05 19:02:19 -0600

    change the way to pass the email to the controller on get active debts

--- src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/infrastructure/web/DebtController.java
+++ src/main/java/com/jimm0063/magi/debt/management/debtmanagementltesystem/infrastructure/web/DebtController.java
@@ -44,8 +44,8 @@ public class DebtController {
         return ResponseEntity.ok(findAllDebtsUseCase.getActiveByDebtAccount(debtAccountCode));
     }
 
-    @GetMapping("/{email}")
-    public ResponseEntity<List<Debt>> getActiveDebts(@PathVariable String email) {
+    @GetMapping("/all")
+    public ResponseEntity<List<Debt>> getActiveDebts(@RequestHeader String email) {
         return ResponseEntity.ok(this.debtRepository.findAllDebtsByUser(email));
     }
 }
